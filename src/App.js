88dac1c6d23c3d02dd6aad4f5656b4edc6a6b77b
tlsrb100 <tlsrb100@gmail.com>
2022-12-26 18:47:27 +0900

    Feat: useTitle 추가
    * title state
    * titleUpdater

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import useTabs from './hooks/useTabs';
-function App() {
-  const content = [
-    {
-      tab: 'Section 1',
-      content: 'I am the content of the Section 1',
-    },
-    {
-      tab: 'Section 2',
-      content: 'I am the content of the Section 2',
-    },
-  ];
-  const { currentItem, changeItem } = useTabs(0, content);
+import { useState, useEffect } from 'react';
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
+  };
+  useEffect(() => updateTitle(), [title]);
+  return setTitle;
+};
+
+function App() {
+  const titleUpdater = useTitle('Loading...');
+  setTimeout(() => titleUpdater('Home'), 3000);
   return (
     <div className='App'>
-      {content.map((section, idx) => (
-        <button key={idx} onClick={() => changeItem(idx)}>
-          {section.tab}
-        </button>
-      ))}
-      <p>{currentItem.content}</p>
+      <div>Hi</div>
     </div>
   );
 }
