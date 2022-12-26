eff0371c710eaad97d3b73462156e08c6a8f8b15
tlsrb100 <tlsrb100@gmail.com>
2022-12-26 19:13:11 +0900

    Feat: useClick 추가
    * useRef로 reference를 리턴
    * useEffect로 reference에 이벤트리스터 추가
    * componentWillunmount때 이벤트리스터 제거

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector('title');
-    htmlTitle.innerText = title;
-  };
-  useEffect(() => updateTitle(), [title]);
-  return setTitle;
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener('click', onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener('click', onClick);
+      }
+    };
+  }, []);
+  return element;
 };
-
 function App() {
-  const titleUpdater = useTitle('Loading...');
-  setTimeout(() => titleUpdater('Home'), 3000);
+  const sayHello = () => {
+    console.log('say Hello');
+  };
+  const title = useClick(sayHello);
   return (
     <div className='App'>
-      <div>Hi</div>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 }
